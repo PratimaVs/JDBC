bf131d47b00be2d8e66a05e0afa7d95e294c4421
PratimaVs <pratimabharti2208@gmail.com>
2024-08-16 02:47:06 -0700

    DML JDBC

---
+++ src/jdbcConnection/DML_Commands.java
@@ -0,0 +1,31 @@
+package jdbcConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DML_Commands {
+
+	public static void main(String[] args) throws SQLException
+	{
+	Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.01:3306/demo","root","Nte#25");
+	Statement stmt = conn.createStatement();
+	String s1 = "Insert into book_detl values('CH003','Nucleuss','Chen','Hilbernn','XYZ','S','1995',3580.0000);";
+	stmt.execute(s1);
+	
+	String s = "select book_no,title,sub_code,author from book_detl";
+	ResultSet rs = stmt.executeQuery(s);
+	while(rs.next())
+	{
+		System.out.print(" B_no: "+ rs.getString("book_no"));
+		System.out.print(" title: "+ rs.getString("title"));
+		System.out.print(" sub_code: "+ rs.getString("sub_code"));
+		System.out.print(" author: "+ rs.getString("author"));
+		System.out.println(" ");
+	}
+	
+	conn.close();
+	}
+}
